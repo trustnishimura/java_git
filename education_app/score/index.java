e526a85428ea5a365c21ff8a7364594377956919
hase <yongdouzhangguchuan14@gmail.com>
2023-11-07 20:44:12 +0900

    合格を追加

--- education_app/score/index.java
+++ education_app/score/index.java
@@ -3,5 +3,6 @@ package education_app.score;
 public class ScoreFeature {
     public static void main(String[] args) {
         System.out.println("これは成績管理機能です");
+        System.out.println("合格です！");
     }
 }
