2c2e6a541363e41360382d7625ff426eea1cc807
西村 海渡 <>
2025-01-28 15:34:53 +0900

    軽微な修正

--- 01_login/index.java
+++ 01_login/index.java
@@ -1,6 +1,6 @@
 public class LoginFeature {
     public static void main(String[] args) {
-        System.out.println("これはログイン機能です");
+        System.out.println("これはログイン機能です2");
 	System.out.println("これはログイン<ブランチを作成した>機能です");
     }
 }
