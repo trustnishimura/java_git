353b40db0c19062487e9e8af11b9d4a5719bee3a
si-mon-isfire <lureshimon@icloud.com>
2025-03-25 14:09:20 +0900

    視聴画面機能です

---
+++ Netflix250325/view/index.java
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg932\cocoartf2821
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset128 HiraginoSans-W4;}
+{\colortbl;\red255\green255\blue255;\red211\green214\blue218;\red2\green2\blue2;}
+{\*\expandedcolortbl;;\cssrgb\c85882\c87059\c88235;\cssrgb\c784\c784\c784\c5882;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \'8e\'8b\'92\'ae\'89\'e6\'96\'ca\'82\'c5\'82\'b7}
